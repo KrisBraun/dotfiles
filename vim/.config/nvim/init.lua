b19374ee177df853e263691e85368c778fe10039
Kris Braun <kris.braun@gmail.com>
2023-02-19 23:00:09 -0500

    Remove ctags

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -170,14 +170,6 @@ require('gitsigns').setup {
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
-    },
-  },
 }
 
 -- Enable telescope fzf native, if installed
