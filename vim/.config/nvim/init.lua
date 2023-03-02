0acf2444326128aa6e78b2400b26275a4e612f62
Kris Braun <kris.braun@gmail.com>
2023-03-01 21:32:04 -0500

    Unimpaired

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -64,6 +64,7 @@ require('packer').startup(function(use)
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
   use 'farmergreg/vim-lastplace'
+  use 'tpope/vim-unimpaired'
 
   -- Fuzzy Finder (files, lsp, etc)
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
