c69c0bdbd4d21a5b4ae7b1b57e7441ac9eb4822d
Kris Braun <kris.braun@gmail.com>
2023-03-08 23:16:36 -0500

    Remove fish iterm and PHP

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,6 @@
 function vi
   nvim $argv
 end
-function composer
-  php ~/bin/composer.phar $argv
-end
-function homestead
-  pushd ~/Homestead; and vagrant $argv; popd
-end
 
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
@@ -14,12 +8,6 @@ if not functions -q fisher
     fish -c fisher
 end
 
-# Set iTerm2 titlebar colour
-echo -n -e "\033]6;1;bg;red;brightness;60\a"
-echo -n -e "\033]6;1;bg;green;brightness;56\a"
-echo -n -e "\033]6;1;bg;blue;brightness;54\a"
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 test -e {$HOME}/.config/fish/config.fish.local ; and source {$HOME}/.config/fish/config.fish.local
 
 for snippet in conf.d/*
