8acd1d6b40fd43dd3c2e004dc5ff802e06954a92
Kris Braun <kris.braun@letscollide.io>
2023-03-22 10:55:40 -0400

    Fix neoformat error on certain filetypes

--- nvim/.config/nvim/lua/kb/plugins.lua
+++ nvim/.config/nvim/lua/kb/plugins.lua
@@ -82,7 +82,7 @@ require('packer').startup(function(use)
   vim.cmd([[
     augroup fmt
       autocmd!
-      autocmd BufWritePre * undojoin | Neoformat
+      au BufWritePre * try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
     augroup END
   ]])
 
